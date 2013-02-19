680e687f3e5dbf27442bc9841b8cfb92f6673762
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-02-19 18:00:01 -0500

    Implement curses-based output for ease of reading test output

--- test_shield.py
+++ test_shield.py
@@ -1,5 +1,21 @@
 import serial
-ser=serial.Serial('/dev/tty.usbmodem621')
+import curses
+from datetime import datetime
+stdscr=curses.initscr()
+stdscr.keypad(1)
+stdscr.nodelay(1)
+stdscr.leaveok(1)
+curses.noecho()
+curses.cbreak()
+
+stdscr=curses.initscr()
+stdscr.keypad(1)
+stdscr.nodelay(1)
+stdscr.leaveok(1)
+
+#ser=serial.Serial('/dev/tty.usbmodem621')
+pad = curses.newpad(100, 100)
+ser=serial.Serial('/dev/tty.usbmodem411')
 
 rpm = 0
 temp = 0
@@ -8,9 +24,13 @@ fuelin = 0
 throttlein = 0
 bmsfault = 0
 clutch = 0
+assist = 0
+brake = 0
 
 
 while 1:
+    if stdscr.getch()==ord('q'):
+        break
     line=ser.readline()
     items=line.split()
     if items[0] == "RPM":
@@ -27,4 +47,22 @@ while 1:
         bmsfault = items[1]
     elif items[0] == "Clutch":
         clutch = items[1]
-    print "RPM\t",rpm,"\tTemp\t",temp,"\tLow Battery\t",battlow,"\tFuelIn\t",fuelin,"\tThrottle\t",throttlein,"\tBMS Fault\t",bmsfault,"\tClutch\t",clutch
+    elif items[0] == "AssistIn":
+        assist = items[1]
+    elif items[0] == "BrakeIn":
+        brake = items[1]
+    pad.addstr(0,0, "RPM:");pad.addstr(0,20,str(rpm))
+    pad.addstr(1,0, "Temp:"); pad.addstr(1,20,str(temp))
+    pad.addstr(2,0, "Low Battery:\t\t" + str(battlow))
+    pad.addstr(3,0, "FuelIn:\t\t" + str(fuelin))
+    pad.addstr(4,0, "Throttle:\t\t" + str(throttlein))
+    pad.addstr(5,0, "BMS Fault:\t\t" + str(bmsfault))
+    pad.addstr(6,0, "Clutch:\t\t" + str(clutch))
+    pad.addstr(7,0, "Assist In:\t\t" + str(assist))
+    pad.addstr(8,0, "Brake In:\t\t" + str(brake))
+    stdscr.refresh()
+    pad.refresh( 0,0, 5,5, 20,75)
+    #print "RPM\t",rpm,"\tTemp\t",temp,"\tLow Battery\t",battlow,"\tFuelIn\t",fuelin,"\tThrottle\t",throttlein,"\tBMS Fault\t",bmsfault,"\tClutch\t",clutch,"\tAssist In\t",assist,"\tBrake In\t",brake
+
+curses.nocbreak(); stdscr.keypad(0); curses.echo()
+curses.endwin()
