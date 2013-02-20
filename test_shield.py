7997c3b0f63ac612281bc0c6575983c9f9917533
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-02-19 21:02:40 -0500

    Format data so that it justiied neatly, thus improving readability.

--- test_shield.py
+++ test_shield.py
@@ -1,6 +1,14 @@
+# The following script communicates the values of inputs into the Arduino shield of the 2013 Bulldogs Racing car.
+# By Alex Carrillo <alejandro.carrillo@yale.edu>
+
 import serial
 import curses
 from datetime import datetime
+
+
+ser=serial.Serial('/dev/tty.usbmodem411')
+#ser=serial.Serial('/dev/tty.usbmodem621')
+
 stdscr=curses.initscr()
 stdscr.keypad(1)
 stdscr.nodelay(1)
@@ -13,9 +21,7 @@ stdscr.keypad(1)
 stdscr.nodelay(1)
 stdscr.leaveok(1)
 
-#ser=serial.Serial('/dev/tty.usbmodem621')
 pad = curses.newpad(100, 100)
-ser=serial.Serial('/dev/tty.usbmodem411')
 
 rpm = 0
 temp = 0
@@ -26,6 +32,7 @@ bmsfault = 0
 clutch = 0
 assist = 0
 brake = 0
+
 
 
 while 1:
@@ -53,16 +60,17 @@ while 1:
         brake = items[1]
     pad.addstr(0,0, "RPM:");pad.addstr(0,20,str(rpm))
     pad.addstr(1,0, "Temp:"); pad.addstr(1,20,str(temp))
-    pad.addstr(2,0, "Low Battery:\t\t" + str(battlow))
-    pad.addstr(3,0, "FuelIn:\t\t" + str(fuelin))
-    pad.addstr(4,0, "Throttle:\t\t" + str(throttlein))
-    pad.addstr(5,0, "BMS Fault:\t\t" + str(bmsfault))
-    pad.addstr(6,0, "Clutch:\t\t" + str(clutch))
-    pad.addstr(7,0, "Assist In:\t\t" + str(assist))
-    pad.addstr(8,0, "Brake In:\t\t" + str(brake))
+    pad.addstr(2,0, "Low Battery:");pad.addstr(2,20,str(battlow))
+    pad.addstr(3,0, "FuelIn:"); pad.addstr(3,20,str(fuelin))
+    pad.addstr(4,0, "Throttle:"); pad.addstr(4,20,str(throttlein))
+    pad.addstr(5,0, "BMS Fault:"); pad.addstr(5,20,str(bmsfault))
+    pad.addstr(6,0, "Clutch:"); pad.addstr(6,20,str(clutch))
+    pad.addstr(7,0, "Assist In:"); pad.addstr(7,20,str(assist))
+    pad.addstr(8,0, "Brake In:"); pad.addstr(8,20,str(brake))
     stdscr.refresh()
     pad.refresh( 0,0, 5,5, 20,75)
     #print "RPM\t",rpm,"\tTemp\t",temp,"\tLow Battery\t",battlow,"\tFuelIn\t",fuelin,"\tThrottle\t",throttlein,"\tBMS Fault\t",bmsfault,"\tClutch\t",clutch,"\tAssist In\t",assist,"\tBrake In\t",brake
 
+# Quit!
 curses.nocbreak(); stdscr.keypad(0); curses.echo()
 curses.endwin()
